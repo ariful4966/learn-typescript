25fa3583375fa6fe7ddcad93dc01b087457d2a72
ariful4082 <ariful4082@gmail.com>
2021-12-05 21:39:35 +0600

    explicit type and union type is on the ts file

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 
 - [x] Lesson 2 : Editor Setup & Compilation 
 - [x] Lesson 3 : Type Basics
+- [x] Lesson 4 : Explicit & Union Types
--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,34 @@
-// object
+// normal variable
 
-let person = {
-    name: 'Mashrafi',
-    age : 35,
-    isCaptain: true
+let b : string | number;
+
+let a: (string | number) [] = []
+
+
+a.push('Ariful')
+
+b = 'Zarif';
+
+a.push('Ariful', 5)
+
+
+// Object
+
+let c: {
+    name: string,
+    age: number,
+    adult: boolean,
+};
+
+c = {
+    name: 'Ariful', 
+    age: 28,
+    adult: true
 }
+c
 
-person.name = "Sakib"
-person
+let c1 : object;
+
+c1 = [1, 3, 5, 'arif'];
+
+c1
